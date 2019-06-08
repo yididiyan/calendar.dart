499c46f601be08c09190db7f6df4584289b9f8fe
Yididya <yididiya.yilma@gmail.com>
2019-06-08 18:59:45 +0300

    Update calendar example

--- example/calendar_example.dart
+++ example/calendar_example.dart
@@ -2,13 +2,30 @@ import 'package:calendar/calendar.dart';
 
 
 void main() {
-  var calendar = new Calendar(
+  // Sunday first calendar 
+  final calendar = new Calendar(
     weekStartDay: 7 // Sunday as start of week
   );
+  
+  // Monday First Calendar
+  final mondayFirstCalendar = new Calendar(
+    weekStartDay: 1
+  );
+  
+  // Calendar Month
+  // Calendar for January, 2019
+  print(calendar.monthDatesCalendar(2019, 1));
+  
+  // Calendar Year
+  // Calendar for Year 1
+  print(calendar.yearDatesCalendar(1));
+  
+  
 
 
+  // Custom Calendar ranging from given start and end dates
   final now = DateTime.now();
   final future = now.add(Duration(days: 75));
+  
   print(calendar.customDatesCalendar(now, future));
-  print(calendar.monthDatesCalendar(2019, 6, day: 8));
 }
